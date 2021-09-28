4e9414cdb36eab9ae302dc9487cbe403490dd838
Welington Gomes <Welingtonsc94@gmail.com>
2021-09-28 15:12:40 -0300

    created activity_main.xml and activity_add_task.xml

---
+++ app/src/main/java/com/welingtongomes/todolist/Activity_add_task.kt
@@ -0,0 +1,11 @@
+package com.welingtongomes.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_add_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
